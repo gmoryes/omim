521cf1b8b35ec7c61ae06619665f46d6f8a403f0
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2018-09-24 19:41:05 +0300

    [routing] TRY FIX FIX FIX

--- routing/index_graph.cpp
+++ routing/index_graph.cpp
@@ -167,6 +167,13 @@ void IndexGraph::GetNeighboringEdges(Segment const & from, RoadPoint const & rp,
     GetNeighboringEdge(
         from, Segment(from.GetMwmId(), rp.GetFeatureId(), rp.GetPointId() - 1, !isOutgoing),
         isOutgoing, edges);
+
+    if (rp.GetPointId() < road.GetPointsCount())
+    {
+      GetNeighboringEdge(
+        Segment(from.GetMwmId(), rp.GetFeatureId(), rp.GetPointId() + 1, isOutgoing), from,
+        isOutgoing, edges);
+    }
   }
 }
 
