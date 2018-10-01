9e49b4eafdc6891a45359c02803c67a7daf6d404
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2018-10-01 10:44:01 +0300

    [routing] debug output

--- generator/routing_index_generator.cpp
+++ generator/routing_index_generator.cpp
@@ -464,7 +464,7 @@ void CalcLandMarks(string const & path, string const & mwmFile, string const & c
 
   map<Segment, map<Segment, RouteWeight>> weights;
 
-  static double constexpr kLandMarksNumberPercent = 5 / 100.0;
+  static double constexpr kLandMarksNumberPercent = 1 / 100.0;
   auto const numExits = connector.GetExits().size();
   auto const step = static_cast<size_t>(1.0 / kLandMarksNumberPercent);
 
