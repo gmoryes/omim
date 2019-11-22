d247c47a60385f5fa3554228784d796525e307d1
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2019-11-22 21:11:57 +0300

    leaps

---
+++ routing/leaps_graph.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "routing/world_graph.hpp"
+
+namespace routing
+{
+class LeapsGraph
+{
+public:
+  LeapsGraph(WorldGraph & worldGraph)
+    : m_worldGraph(worldGraph), m_xxxxxxxxxxxxxxxx(), m_yyyyyyyyyyy(), m_zzzzzzz()
+  {
+  }
+
+private:
+  WorldGraph & m_worldGraph;
+  int m_xxxxxxxxxxxxxxxx;
+  int m_yyyyyyyyyyy;
+  int m_zzzzzzz;
+};
+}  // namespace routing
